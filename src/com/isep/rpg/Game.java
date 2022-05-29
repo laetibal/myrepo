ac3f1f6ca9d303a8e84beb6f88e0abf8d9a675a2
LaetiBal <laba60918@eleve.isep.fr>
2022-05-29 14:30:49 +0200

    Add .gitignore

--- src/com/isep/rpg/Game.java
+++ src/com/isep/rpg/Game.java
@@ -1,20 +1,40 @@
 package com.isep.rpg;
 import java.util.*;
+import java.util.Scanner;
 
 import com.isep.rpg.Hero;
 import com.isep.utils.InputParser;
 
-public class Game{
+import static com.isep.rpg.Food.*;
+import static com.isep.rpg.Potion.*;
+
+public class Game {
+
+    protected static ArrayList<String> inventaire = new ArrayList<String>();
+    protected static ArrayList<String> inventaireRemove = new ArrayList<String>();
 
     public static Scanner scanner = new Scanner(System.in);
     public static Scanner sc = new Scanner(System.in);
     public static Scanner sn = new Scanner(System.in);
+    public static Scanner sa = new Scanner(System.in);
     public static Scanner sar = new Scanner(System.in);
+    public static Scanner so = new Scanner(System.in);
+    public static Scanner se = new Scanner(System.in);
+    public static Scanner sco = new Scanner(System.in);
+    public static Scanner scc = new Scanner(System.in);
+    public static Scanner sii = new Scanner(System.in);
 
     protected static List<Hero> heroes;
     protected static int playerTurn;
     protected static InputParser inputParser;
     protected static int choix;
+    protected static int choixConsumable;
+    protected static int choixPotion;
+    protected static int choixNourriture;
+    protected static int choixPotion2;
+    protected static int choixNourriture2;
+    protected static int choixInventaire;
+    protected static int action;
 
 
     public static void clearConsole(){
@@ -38,9 +58,9 @@ public class Game{
         boolean nameSet = false;
         String name;
         clearConsole();
-        printSeparator(40);
+        printSeparator(20);
         System.out.println("Mini RPG Lite 3000");
-        printSeparator(40);
+        printSeparator(20);
         anythingToContinue();
 
         do{
@@ -72,5 +92,125 @@ public class Game{
             Healer.nom=sn.nextLine();
             //System.out.println("New name : "+Healer.nom);
         }
+        do {
+            menu();
+            action = sa.nextInt();
+            action(action);
+        } while (action!=4);
         }
+    public static void menu(){
+
+        System.out.println(" ");
+        System.out.println("\tFaites votre choix :\n" +
+                "(1) Ajouter un consumable\n" +
+                "(2) Choisir une potion\n" +
+                "(2) Choisir de la nourriture\n" +
+                "(4) Quitter le jeu");
+        System.out.println(" ");
+
+    }
+
+    public static int action(int action){
+        switch (action){
+            case 1:
+                creerInventaires(inventaire);
+                break;
+
+            case 2 :
+                choisirPotion();
+                break;
+
+            case 3 :
+                choisirNourriture();
+                break;
+
+        }
+        return choix;
+    }
+
+    public static ArrayList<String> creerInventaires(ArrayList<String> inventaire) {
+
+        System.out.println("Quel consumable souhaitez-vous ajouter ?\n" +
+                "(1) Potion\n" +
+                "(2) Nourriture\n");
+        choixConsumable=so.nextInt();
+
+        if (choixConsumable==1){
+
+            System.out.println("Quel potion souhaitez-vous ajouter ?\n" +
+                    "(1) Potion du dragon\n" +
+                    "(2) Potion de vie\n" +
+                    "(3) Potion de feu\n");
+            choixPotion=se.nextInt();
+
+            if (choixPotion==1){
+                System.out.println("Potion du dragon ajoutée !");
+                inventaire.add(PotionDuDragon.nom);
+            }
+
+            else if(choixPotion==2){
+                System.out.println("Potion de vie ajoutée !");
+                inventaire.add(PotionDeVie.nom);
+            }
+
+            else if(choixPotion==3){
+                System.out.println("Potion de feu ajoutée !");
+                inventaire.add(PotionDeFeu.nom);
+            }
+        }
+
+        else{
+            System.out.println("Quel nourriture souhaitez-vous ajouter ?\n" +
+                    "(1) Lembas d'or\n" +
+                    "(2) Lembas de cuivre\n" +
+                    "(3) Lembas de sable\n");
+            choixNourriture=sco.nextInt();
+
+            if (choixNourriture==1){
+                System.out.println("Lembas d'or ajoutée !");
+                inventaire.add(LembasdOr.nom);
+            }
+
+            else if(choixNourriture==2){
+                System.out.println("Lembas de cuivre ajoutée !");
+                inventaire.add(LembasDeCuivre.nom);
+            }
+
+            else if(choixNourriture==3){
+                System.out.println("Lembas de sable ajoutée !");
+                inventaire.add(LembasDeSable.nom);
+            }
+        }
+        System.out.println("Voila votre inventaire : "+inventaire);
+        return inventaire;
+        }
+
+    public static void choisirPotion(){
+        System.out.println(" ");
+        System.out.println("Quel potion souhaitez-vous utiliser ?\n");
+
+        for (int i =0; i<inventaire.size();i++){
+            System.out.println("("+i+")"+" "+inventaire.get(i));
+        }
+
+        choixPotion2=scc.nextInt();
+
+        System.out.println("Vous avez consommé "+inventaire.get(choixInventaire)+" !");
+        inventaire.remove(choixPotion2);
+    }
+
+    public static void choisirNourriture(){
+        System.out.println(" ");
+        System.out.println("Quel nourriture souhaitez-vous utiliser ?\n");
+
+        for (int i =0; i<inventaire.size();i++){
+            System.out.println("("+i+")"+" "+inventaire.get(i));
+        }
+
+        choixNourriture2=sii.nextInt();
+
+        System.out.println("Vous avez consommé "+inventaire.get(choixInventaire)+" !");
+        inventaire.remove(choixNourriture2);
+    }
+
     }
--- src/com/isep/rpg/Potion.java
+++ src/com/isep/rpg/Potion.java
@@ -1,4 +1,21 @@
 package com.isep.rpg;
 
 public class Potion implements Consumable{
+    protected static String nom;
+    protected static int lifePoints;
+
+    protected static Potion PotionDuDragon = new Potion();
+    protected static Potion PotionDeFeu = new Potion();
+    protected static Potion PotionDeVie = new Potion();
+
+    public Potion() {
+        PotionDuDragon.nom="Potion Du Dragon";
+        PotionDuDragon.lifePoints=30;
+
+        PotionDeFeu.nom="Potion De Feu";
+        PotionDeFeu.lifePoints=25;
+
+        PotionDeVie.nom="Potion De Vie";
+        PotionDeVie.lifePoints=35;
+    }
 }
